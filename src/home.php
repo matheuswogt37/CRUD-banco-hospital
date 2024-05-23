cbc433d539fc4b65e9c03151c59393cffc306921
matheuswogt37 <matheusnotas07@gmail.com>
2024-05-23 11:26:06 -0300

    exclui o admin

--- src/home.php
+++ src/home.php
@@ -6,8 +6,6 @@
 <body>
     <main>
         <h1>Hospital - home</h1>
-        <button><a href="admin">admin</a></button>
-        </br>
         <button><a href="acompanhante">acompanhante</a></button>
         </br>
     </main>
